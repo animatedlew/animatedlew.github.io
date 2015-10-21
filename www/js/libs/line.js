684eec235e04690f1272168d02ee91e93c508e9a
Lewis Moronta <lewis.moronta@gmail.com>
2015-10-20 22:03:34 -0400

    adds chainedLineSegments to index

--- www/js/libs/line.js
+++ www/js/libs/line.js
@@ -14,14 +14,14 @@ class Line {
         this.ctx.stroke();
         this.ctx.restore();
     }
-    render(x1, y1, x2, y2) {
+    render(x1, y1, x2, y2, color) {
         const a = new Vector(x1, y1);
         const b = new Vector(x2, y2);
         var c = a.clone().sub(b);
         var opacity = 1 - c.mag / this.weakeningDistance;
         opacity = Math.max(Math.min(opacity, 1), 0);
         c.add(a);
-        this.draw(a.x, a.y, b.x, b.y, `rgba(0, 0, 0, ${opacity})`);
+        this.draw(a.x, a.y, b.x, b.y, color || `rgba(0, 0, 0, ${opacity})`);
     }
 }
 
